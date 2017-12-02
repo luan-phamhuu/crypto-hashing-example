909d4b4a75dc0ed06cc7ac039209eb92e08ce480
Luan <phluan92@gmail.com>
2017-12-02 15:41:24 +0700

    hidding property demo

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 int main(int argc, const char * argv[]) {
-    std::string src_str = "1";
+    std::string src_str = "hkadhfkdsfgdfk.12313";
     std::string hash_hex_str = picosha2::hash256_hex_string(src_str);
     cout << hash_hex_str << "\n";
     
-    
+    for (int i = 0; i <= pow(10, 6); i++) {
+      if (picosha2::hash256_hex_string(to_string(i)) == hash_hex_str) {
+        cout << i;
+        break;
+      }
+    }    
     
     
     
